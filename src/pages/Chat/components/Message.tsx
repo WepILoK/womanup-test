793a0c34691428d35452df0ea724b1a4ca1e97d6
WepILoK <artur.nasyrov1999@mail.ru>
2021-07-10 18:43:46 +0300

    Fix

--- src/pages/Chat/components/Message.tsx
+++ src/pages/Chat/components/Message.tsx
@@ -6,12 +6,12 @@ interface IMessage {
     text: string
 }
 
-export const Message: React.FC<IMessage> = ({name,text}) => {
+export const Message: React.FC<IMessage> = React.memo(({name,text}) => {
     return (
         <div className='message'>
             <div className='user-name'>{name}</div>
             <div className='text'>{text}</div>
         </div>
     );
-};
+});
 
